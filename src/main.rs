67b917b19a667833b9b8e6d576b71e58e2a8322d
Sumeet Agarwal <sumeet.a@gmail.com>
2023-10-01 00:02:55 -0700

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,36 @@
+#![feature(iter_array_chunks)]
+
 use psimple::Simple;
 use pulse::sample::{Format, Spec};
 use pulse::stream::Direction;
 
-fn main() {
-    const SAMPLE_RATE: f32 = 44100.0; // 44.1 kHz
-    const FREQUENCY: f32 = 440.0; // Frequency of A4 note in Hz
-    const DURATION: f32 = 8.0; // Duration in seconds
-    const BUFFER_SIZE: usize = 1024; // Fixed buffer size
+const SAMPLE_RATE: f32 = 44100.0; // 44.1 kHz
+const BUFFER_SIZE: usize = 1024;
+const BUFFER_SIZE_HALF: usize = BUFFER_SIZE / 2;
 
+fn note(duration_ms: usize, freq: f32) -> impl Iterator<Item = f32> {
+    let samples_per_note = (SAMPLE_RATE * duration_ms as f32 / 1000.0) as usize;
+    let mut sample_count = 0;
+
+    let phase_increment = 2.0 * std::f32::consts::PI * freq / SAMPLE_RATE;
+    let mut phase: f32 = 0.0;
+
+    std::iter::from_fn(move || {
+        if sample_count < samples_per_note {
+            let sample = phase.sin();
+            phase += phase_increment;
+            if phase >= 2.0 * std::f32::consts::PI {
+                phase -= 2.0 * std::f32::consts::PI;
+            }
+            sample_count += 1;
+            Some(sample)
+        } else {
+            None
+        }
+    })
+}
+
+fn main() {
     let spec = Spec {
         format: Format::F32le,
         channels: 2,
@@ -26,34 +49,15 @@ fn main() {
         None,                // Use default buffering attributes
     )
     .unwrap();
-
-    let samples_per_note = (SAMPLE_RATE * DURATION) as usize;
-    let mut sample_count = 0;
-
-    let mut buf = [0.0f32; BUFFER_SIZE];
-    let phase_increment = 2.0 * std::f32::consts::PI * FREQUENCY / SAMPLE_RATE;
-    let mut phase: f32 = 0.0;
-
-    'main: loop {
-        for i in 0..BUFFER_SIZE {
-            if sample_count < samples_per_note {
-                buf[i] = phase.sin();
-                phase += phase_increment;
-                if phase >= 2.0 * std::f32::consts::PI {
-                    phase -= 2.0 * std::f32::consts::PI;
-                }
-                sample_count += 1;
-            } else {
-                break 'main;
-            }
+    let a440_left = note(5_000, 440.);
+    let a440_right = note(5_000, 440.);
+    let mut buffer = [0f32; BUFFER_SIZE];
+    for chunks in a440_left.zip(a440_right).array_chunks::<BUFFER_SIZE_HALF>() {
+        for (i, (left, right)) in chunks.iter().enumerate() {
+            buffer[i * 2] = *left;
+            buffer[i * 2 + 1] = *right;
         }
-
-        if sample_count >= samples_per_note {
-            sample_count = 0; // Reset for the next note
-        }
-
-        // Now you can send `buf` to PulseAudio for playback
-        s.write(as_u8_slice(&buf)).unwrap();
+        s.write(as_u8_slice(&buffer)).unwrap();
     }
 }
 
